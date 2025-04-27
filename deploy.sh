259c21b52f08b7b8615393ac45b4c60625488102
Hector Gabucio <hectorcompangabucio@gmail.com>
2025-04-27 10:35:34 +0200

    feat: add new projects

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-docker run --rm -it \
-  -v $(pwd):/src \
-  klakegg/hugo:0.59.0
+hugo
 
 # Go To Public folder
 cd public
